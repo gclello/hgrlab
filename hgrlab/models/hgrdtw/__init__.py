0c062d93a59c0ac436bde629707a3de50a95989c
Guilherme De Lello <gclello@gmail.com>
2024-04-22 22:10:54 -0400

    Add feature_extraction stage of the hgrdtw model

--- hgrlab/models/hgrdtw/__init__.py
+++ hgrlab/models/hgrdtw/__init__.py
@@ -1,7 +1,12 @@
 from .preprocessing import preprocess
 from .segmentation import segment
 
+from .feature_extraction import extract_training_features
+from .feature_extraction import extract_test_features
+
 __all__ = [
     "preprocess",
     "segment",
+    "extract_training_features",
+    "extract_test_features",
 ]
---
+++ hgrlab/models/hgrdtw/feature_extraction.py
@@ -0,0 +1,260 @@
+import datetime
+
+import numpy as np
+import scipy
+import scipy.signal
+
+import fastdtw
+
+import emgts
+from .segmentation import get_activity_indices
+from .segmentation import get_activity_indices_from_trial_set
+from .segmentation import get_activity_indices_from_trial_set_windows
+from .preprocessing import preprocess
+
+def dtw_from_all_trials(trials, trials_indices):
+    '''Compute DTW distances among all trials'''
+
+    number_of_trials = len(trials)
+    dtw_matrix = np.zeros((number_of_trials, number_of_trials))
+    
+    for i in np.arange(0, trials.shape[0]):
+        for j in np.arange(0, trials.shape[0]):
+            if j > i:
+                distance, path = fastdtw.fastdtw(
+                    trials[i][trials_indices[i][0]:trials_indices[i][1]],
+                    trials[j][trials_indices[j][0]:trials_indices[j][1]],
+                    radius=100,
+                    dist=2
+                )
+                
+                dtw_matrix[i, j] = dtw_matrix[j, i] = distance
+                
+    return dtw_matrix
+
+def get_class_center_trial_id(trials, trials_indices, trials_labels, class_label):
+    '''Get the trial_id that is the nearest among all other trial_ids from the same class'''
+
+    dtw_matrix = dtw_from_all_trials(
+        trials[trials_labels == class_label],
+        trials_indices[trials_labels == class_label]
+    )
+
+    min_cost_index = dtw_matrix.sum(axis=0).argmin()
+    return np.argwhere(trials_labels == class_label)[min_cost_index][0]
+
+def dtw_from_specific_trial_ids(trials, trials_indices, trial_ids):
+    '''Compute DTW distances between each trial and specific trials'''
+
+    number_of_trials = len(trials)
+    number_of_trial_ids = len(trial_ids)
+    dtw_matrix = np.zeros((number_of_trials, number_of_trial_ids))
+    
+    for i, trial_data in enumerate(trials):
+        for j, trial_id in enumerate(trial_ids):
+            distance, path = fastdtw.fastdtw(
+                trial_data[trials_indices[i][0]:trials_indices[i][1]],
+                trials[trial_id][trials_indices[trial_id][0]:trials_indices[trial_id][1]],
+                radius=100,
+                dist=2
+            )
+                  
+            dtw_matrix[i, j] = distance
+                
+    return dtw_matrix
+
+def dtw_between_two_series(
+    series1,
+    series1_indices,
+    series1_ids,
+    series2,
+    series2_indices,
+    series2_ids,
+    series1_preprocess=None,
+    series1_sampling_rate=None,
+    series2_preprocess=None,
+    series2_sampling_rate=None,
+):
+    '''Compute DTW distances between two series'''
+    
+    dummy_preprocess = lambda data, _: data
+    
+    if series1_preprocess is None:
+        series1_preprocess = dummy_preprocess
+        
+    if series2_preprocess is None:
+        series2_preprocess = dummy_preprocess
+    
+    series1_length = len(series1_ids)
+    series2_length = len(series2_ids)
+    
+    dtw_matrix = np.zeros((series1_length, series2_length))
+    
+    for i, series1_id in enumerate(series1_ids):
+        for j, series2_id in enumerate(series2_ids):
+            distance, path = fastdtw.fastdtw(
+                series1_preprocess(
+                    series1[series1_id][series1_indices[series1_id][0]:series1_indices[series1_id][1]],
+                    series1_sampling_rate
+                ),
+                series2_preprocess(
+                    series2[series2_id][series2_indices[series2_id][0]:series2_indices[series2_id][1]],
+                    series2_sampling_rate,
+                ),
+                radius=100,
+                dist=2,
+            )
+
+            dtw_matrix[i, j] = distance
+                
+    return dtw_matrix
+
+def extract_training_features(config):
+    '''Compute features from training feature set'''
+
+    start_ts = datetime.datetime.now()
+    
+    user_id = config['user_id']
+    dataset_path = config['database_path']
+    dataset_type = config['database_type']
+    
+    stft_window_length = config['stft_window_length']
+    stft_window_overlap = config['stft_window_overlap']
+    stft_nfft = config['stft_nfft']
+    
+    activity_threshold = config['activity_threshold']
+    activity_extra_samples = config['activity_extra_samples']
+    activity_min_length = config['activity_min_length']
+    
+    trial_set = emgts.EmgTrialSet(dataset_path, user_id, dataset_type)
+    trial_set_labels = trial_set.get_all_trials_labels()
+    
+    stft_window = scipy.signal.windows.hamming(stft_window_length)
+    
+    activity_indices, preprocessed_data, data = get_activity_indices_from_trial_set(
+        trial_set,
+        stft_window=stft_window,
+        stft_overlap=stft_window_overlap,
+        stft_nfft=stft_nfft,
+        threshold=activity_threshold,
+        extra_samples=activity_extra_samples,
+        min_length=activity_min_length,
+        get_activity_indices=get_activity_indices,
+        preprocess=preprocess,
+    )
+    
+    class_centers = {}
+    
+    for gesture in trial_set.labels:
+        class_centers[gesture] = get_class_center_trial_id(
+            preprocessed_data,
+            activity_indices,
+            trial_set_labels,
+            gesture,
+        )
+    
+    class_centers_trial_ids = [class_centers[key] for key in class_centers]
+    
+    dtw_matrix = dtw_from_specific_trial_ids(
+        preprocessed_data,
+        activity_indices,
+        class_centers_trial_ids,
+    )
+    
+    mean = dtw_matrix.mean(axis=1)
+    std = dtw_matrix.std(axis=1, ddof=1)
+    dtw_matrix_normalized = ((dtw_matrix.T - mean) / std).T
+    
+    end_ts = datetime.datetime.now()
+        
+    result = {
+        'processing_time': end_ts - start_ts,
+        'features': dtw_matrix_normalized,
+        'labels': trial_set_labels,
+        'preprocessed_data': preprocessed_data,
+        'activity_indices': activity_indices,
+        'class_centers': class_centers,
+    }
+    
+    return result
+
+def extract_test_features(config):
+    '''Compute features from test feature set'''
+
+    start_ts = datetime.datetime.now()
+    
+    user_id = config['user_id']
+    dataset_path = config['database_path']
+    dataset_type = config['database_type']
+    
+    stft_window_length = config['stft_window_length']
+    stft_window_overlap = config['stft_window_overlap']
+    stft_nfft = config['stft_nfft']
+    
+    activity_threshold = config['activity_threshold']
+    activity_extra_samples = config['activity_extra_samples']
+    activity_min_length = config['activity_min_length']
+    
+    feature_window_length = config['feature_window_length']
+    feature_overlap_length = config['feature_overlap_length']
+    
+    training_data = config['training_data']
+    training_preprocessed_data = training_data['preprocessed_data']
+    training_activity_indices = training_data['activity_indices']
+    training_class_centers = training_data['class_centers']
+
+    trial_set = emgts.EmgTrialSet(dataset_path, user_id, dataset_type)
+    trial_set_labels = trial_set.get_all_trials_labels()
+    sampling_rate = trial_set.sampling_rate
+    
+    stft_window = scipy.signal.windows.hamming(stft_window_length)
+    
+    test_activity_indices, test_windows_preprocessed, test_windows = get_activity_indices_from_trial_set_windows(
+        trial_set=trial_set,
+        window_length=feature_window_length,
+        overlap_length=feature_overlap_length,
+        stft_window=stft_window,
+        stft_overlap=stft_window_overlap,
+        stft_nfft=stft_nfft,
+        threshold=activity_threshold,
+        extra_samples=activity_extra_samples,
+        min_length=activity_min_length,
+        get_activity_indices=get_activity_indices,
+        preprocess=preprocess,
+    )
+    
+    training_class_centers_trial_ids = [training_class_centers[key] for key in training_class_centers]
+    
+    trial_set_windows_dtw_matrix = np.zeros((
+        test_activity_indices.shape[0],
+        test_activity_indices.shape[1],
+        len(training_class_centers_trial_ids),
+    ))
+    
+    for test_trial_id, preprocessed_data in enumerate(test_windows_preprocessed):
+        dtw_matrix = dtw_between_two_series(
+            series1=test_windows[test_trial_id],
+            series1_indices=test_activity_indices[test_trial_id],
+            series1_ids=np.arange(0, test_windows.shape[1]),
+            series2=training_preprocessed_data,
+            series2_indices=training_activity_indices,
+            series2_ids=training_class_centers_trial_ids,
+            series1_preprocess=preprocess,
+            series1_sampling_rate=sampling_rate,
+        )
+    
+        mean = dtw_matrix.mean(axis=1)
+        std = dtw_matrix.std(axis=1, ddof=1)
+    
+        dtw_matrix_normalized = ((dtw_matrix.T - mean) / std).T
+    
+        trial_set_windows_dtw_matrix[test_trial_id,:,:] = dtw_matrix_normalized
+        
+    end_ts = datetime.datetime.now()
+        
+    return {
+        'processing_time': end_ts - start_ts,
+        'features': trial_set_windows_dtw_matrix,
+        'labels': trial_set_labels,
+        'activity_indices': test_activity_indices,
+    }
