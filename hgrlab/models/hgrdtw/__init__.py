0305e06e73f4e7611bb073a3e10c71758d9569d6
Guilherme De Lello <gclello@gmail.com>
2024-04-23 01:33:56 -0400

    Add cross validation module of the hgrdtw model

--- hgrlab/models/hgrdtw/__init__.py
+++ hgrlab/models/hgrdtw/__init__.py
@@ -6,12 +6,16 @@ from .feature_extraction import extract_test_features
 
 from .classification import build_classifier, fit, predict
 
+from .cross_validation import train_val_split, k_fold_classification_cost
+
 __all__ = [
-    "preprocess",
-    "segment",
-    "extract_training_features",
-    "extract_test_features",
-    "build_classifier",
-    "fit",
-    "predict",
+    'preprocess',
+    'segment',
+    'extract_training_features',
+    'extract_test_features',
+    'build_classifier',
+    'fit',
+    'predict',
+    'train_val_split',
+    'k_fold_classification_cost',
 ]
---
+++ hgrlab/models/hgrdtw/cross_validation.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+from .feature_extraction import extract_training_features
+from .classification import build_classifier, fit, predict
+
+def train_val_split(X, y, val_size_per_class, offset=0):
+    validation_indices = get_validation_indices(
+        y,
+        limit_per_class=val_size_per_class,
+        offset=offset,
+    )
+
+    X_train = X[validation_indices == False]
+    y_train = y[validation_indices == False]
+
+    X_val = X[validation_indices == True]
+    y_val = y[validation_indices == True]
+
+    return X_train, y_train, X_val, y_val
+
+def get_validation_indices(labels, limit_per_class=2, offset=0):
+    unique_labels = np.unique(labels)
+    validation_indices = []
+    
+    for label in unique_labels:
+        label_indices = np.argwhere(labels == label).flatten()
+        label_validation_indices = np.roll(label_indices, -offset)[0:limit_per_class]
+        validation_indices = validation_indices + label_validation_indices.tolist()
+    
+    is_validation = np.full(len(labels), False)
+    is_validation[validation_indices] = True
+        
+    return is_validation
+
+def k_fold_classification_cost(
+        config,
+        classifier_name,
+        folds,
+        val_size_per_class
+):
+    result = extract_training_features(config)
+    features = result["features"]
+    labels = result["labels"]
+    
+    total_erros = 0
+    
+    for offset in np.arange(0, folds):
+        X_train, y_train, X_val, y_val = train_val_split(
+            X=features,
+            y=labels,
+            val_size_per_class=val_size_per_class,
+            offset=offset,
+        )
+    
+        model = build_classifier(classifier_name)
+        fit(model, X_train, y_train)
+        predictions = predict(model, X_val)
+    
+        correct_predictions = (predictions == y_val)
+        errors = np.size(y_val) - np.count_nonzero(correct_predictions)
+        total_erros = total_erros + errors
+    
+    return total_erros
