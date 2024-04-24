346427c7f7b9222099c406dca8d412abe19f15f0
Guilherme De Lello <gclello@gmail.com>
2024-04-24 00:28:31 -0400

    Add utils.AssetManager

--- hgrlab/utils/__init__.py
+++ hgrlab/utils/__init__.py
@@ -1,8 +1,10 @@
 from .sliding_window import sliding_window
 from .pickle import save_pickle, load_pickle
+from .asset_manager import AssetManager
 
 __all__ = [
     'sliding_window',
     'save_pickle',
     'load_pickle',
+    'AssetManager',
 ]
---
+++ hgrlab/utils/asset_manager.py
@@ -0,0 +1,48 @@
+import os
+import tempfile
+import urllib.request
+
+class AssetManager:
+    '''Remote assets catalog manager'''
+    
+    def __init__(self):
+        self.remote_assets = {}
+        self.local_assets = {}
+        
+    def __str__(self):
+        return '[AssetManager] Remote assets: %d | Local assets: %d' % (
+            len(self.remote_assets.keys()),
+            len(self.local_assets.keys()),
+        )
+    
+    def add_remote_asset(self, key, remote_id, filename):
+        self.remote_assets[key] = {
+            "remote_id": remote_id,
+            "filename": filename,
+        }
+
+    def get_asset_url(self, key):
+        remote_id = self.remote_assets[key]['remote_id']
+        return 'https://drive.google.com/uc?export=download&id=%s' % remote_id
+    
+    def get_asset_path(self, key):
+        filename = self.remote_assets[key]['filename']
+        base_dir = os.path.join(tempfile.gettempdir(), 'hgrlab')
+        if not os.path.isdir(base_dir):
+            os.mkdir(base_dir)
+        return os.path.join(base_dir, filename)
+    
+    def download_asset(self, key):
+        path = self.get_asset_path(key)
+        url = self.get_asset_url(key)
+
+        if not os.path.isfile(path):
+            urllib.request.urlretrieve(
+                url,
+                path,
+            )
+
+        if os.path.isfile(path):
+            self.local_assets[key] = self.remote_assets[key]
+        
+        return path
