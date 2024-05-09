59867d6967f6c2ba3ba3f866100a3cf081b85705
Guilherme De Lello <gclello@gmail.com>
2024-05-09 11:27:45 -0400

    Move experiments output functions to experiments.__init__.py

--- hgrlab/experiments/delello_lnlm2024.py
+++ hgrlab/experiments/delello_lnlm2024.py
@@ -10,39 +10,7 @@ from ..models.hgrdtw import build_classifier, fit, predict
 from ..utils import load_pickle
 from ..utils import AssetManager
 
-def get_formatted_time():
-    now = datetime.datetime.now()
-    return '{YEAR:04d}-{MONTH:02d}-{DAY:02d} {HOUR:02d}:{MINUTE:02d}:{SECOND:02d}'.format(
-        YEAR=now.year,
-        MONTH=now.month,
-        DAY=now.day,
-        HOUR=now.hour,
-        MINUTE=now.minute,
-        SECOND=now.second,
-    )
-
-def print_message(message):
-    print('{DATE} {MESSAGE}'.format(
-        DATE=get_formatted_time(),
-        MESSAGE=message,
-    ))
-
-def print_title(message):
-    print(message)
-
-def print_result(message):
-    print(message)
-
-def print_line_break():
-    print()
-
-def print_progress(task, progress, status):
-    print('{DATE} [{PROGRESS:5.1f}%] {TASK}: {STATUS}'.format(
-        DATE=get_formatted_time(),
-        TASK=task,
-        PROGRESS=progress*100,
-        STATUS=status,
-    ))
+from ..experiments import print_message, print_title, print_progress, print_result, print_line_break
 
 def find_optimum_segmentation_threshold(config):
     threshold_min = config['threshold_min']
