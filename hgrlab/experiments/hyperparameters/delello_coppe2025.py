561b57b4191bacea7b60dcb53379fab6c3843e34
Guilherme De Lello <gclello@gmail.com>
2025-02-08 00:59:23 -0500

    Add generate_dt_options

--- hgrlab/experiments/hyperparameters/delello_coppe2025.py
+++ hgrlab/experiments/hyperparameters/delello_coppe2025.py
@@ -110,6 +110,17 @@ def generate_knn_options():
     
     return options
 
+def generate_dt_options():
+    criteria = ['gini', 'entropy', 'log_loss']
+    options = []
+    
+    for criterion in criteria:
+        options.append({
+            'criterion': criterion,
+        })
+    
+    return options
+
 def generate_twsd_options():
     address_sizes = np.arange(4, 49, 4)
     thermometer_size_powers = np.arange(3, 15)
